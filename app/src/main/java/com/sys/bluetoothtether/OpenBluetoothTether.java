48df7c586e92022f05f97d94919597069d509048
Feng <oyf_feng@163.com>
2023-02-14 08:20:21 +0800

    添加循环判断1分钟

--- app/src/main/java/com/sys/bluetoothtether/OpenBluetoothTether.java
+++ app/src/main/java/com/sys/bluetoothtether/OpenBluetoothTether.java
@@ -13,16 +13,32 @@ public class OpenBluetoothTether {
     public static final String TAG = "OpenBluetoothTether";
 
     private AtomicReference<Object> mBluetoothPan = new AtomicReference<>();
+    private volatile boolean connect = false;
     private BluetoothProfile.ServiceListener mProfileServiceListener = new BluetoothProfile.ServiceListener() {
         @Override
         public void onServiceConnected(int i, BluetoothProfile bluetoothProfile) {
+            connect = true;
             Log.i(TAG, "onServiceConnected");
             mBluetoothPan.set(bluetoothProfile);
-            openBluetoothShare();
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (connect) {
+                        openBluetoothShare();
+                        try {
+                            Log.d(TAG, "OpenBluetoothTether: Thread.sleep(60 * 1000)");
+                            Thread.sleep(60 * 1000);
+                        } catch (Throwable e) {
+
+                        }
+                    }
+                }
+            }).start();
         }
 
         @Override
         public void onServiceDisconnected(int i) {
+            connect = false;
             Log.i(TAG, "onServiceDisconnected");
         }
     };
@@ -39,10 +55,13 @@ public class OpenBluetoothTether {
             Class bluetoothPanClass = Class.forName("android.bluetooth.BluetoothPan");
             if (bluetoothPan != null) {
                 Method methodIsTethering = bluetoothPanClass.getMethod("isTetheringOn");
-                Log.i(TAG, "isTetheringOn = " + methodIsTethering.invoke(bluetoothPan));
-                Method methodSetBluetoothTethering = bluetoothPanClass.getMethod("setBluetoothTethering", boolean.class);
-                methodSetBluetoothTethering.invoke(bluetoothPan, true);
-                Log.i(TAG, "isTetheringOn = " + methodIsTethering.invoke(bluetoothPan));
+                boolean open = (boolean) methodIsTethering.invoke(bluetoothPan);
+                Log.i(TAG, "isTetheringOn = " + open);
+                if (!open) {
+                    Method methodSetBluetoothTethering = bluetoothPanClass.getMethod("setBluetoothTethering", boolean.class);
+                    methodSetBluetoothTethering.invoke(bluetoothPan, true);
+                    Log.i(TAG, "isTetheringOn = " + methodIsTethering.invoke(bluetoothPan));
+                }
             }
         } catch (Exception e) {
             Log.e(TAG, "openBluetoothShare: ", e);
